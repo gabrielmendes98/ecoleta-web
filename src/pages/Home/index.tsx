2a270ac97c3dfc1aaf1bde1d2b11d4d4c8964349
Gabriel <gabrielmssantiago@gmail.com>
2020-06-06 23:50:28 -0300

    home page done

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import './styles.css';
 import logo from '../../assets/logo.svg';
 
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiSearch } from 'react-icons/fi';
 
 const Home = () => {
   return (
@@ -12,17 +12,23 @@ const Home = () => {
       <div className="content">
         <header>
           <img src={logo} alt="Ecoleta" />
+          <Link to="/create-point">
+            <span>
+              <FiLogIn />
+            </span>
+            <strong>Cadastre um ponto de coleta</strong>
+          </Link>
         </header>
 
         <main>
           <h1>Seu marketplace de coleta de resíduos.</h1>
           <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
 
-          <Link to="/create-point">
+          <Link to="/search-point">
             <span>
-              <FiLogIn />
+              <FiSearch />
             </span>
-            <strong>Cadastre um ponto de coleta</strong>
+            <strong>Pesquisar pontos de coleta</strong>
           </Link>
         </main>
       </div>
